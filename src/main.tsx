836c4bfbacd78d172d39da7763f820f04197a519
Mily Moreno <milydemendoza@gmail.com>
2023-09-16 12:18:45 -0400

    Configurar archivo main

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Como estamos usando client-side-rendering es posible que el elemento root no esté disponible por alguna situación, creamos esta validacion para saber en caso de que suceda.
+const renderApp = (rootElement: HTMLElement | null) => {
+  if (rootElement === null) {
+    console.error("El elemento con ID 'root' no se encontró en el documento.")
+    return
+  }
+
+  const mainApp = (
+    <React.Suspense>
+      <App />
+    </React.Suspense>
+  )
+
+  const rootInstance = ReactDOM.createRoot(rootElement)
+  rootInstance.render(mainApp)
+}
+
+const root = document.getElementById('root')
+renderApp(root)
