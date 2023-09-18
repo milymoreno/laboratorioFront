ad7161dc9d074d1c35225ad9a3f36cb392566444
Mily Moreno <milydemendoza@gmail.com>
2023-09-17 21:44:04 -0400

    cambiar apiURl

--- src/store/patients/actions.ts
+++ src/store/patients/actions.ts
@@ -5,7 +5,7 @@ import { type IPatientStore } from './types'
 import axios from 'axios'
 import { type IResponseGetAllPatients } from '../types'
 
-const apiURL = 'http://127.0.0.1:3333'
+const apiURL = 'https://laboratorio-back.onrender.com/patients'
 
 // Actions
 const { setState } = usePatientStore
